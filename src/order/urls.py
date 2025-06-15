0d3ac4ca1cae2016558fec3bacdcadab1acbd19d
muhammadaminusmonov <muhammadamincoder@gmail.com>
2025-06-16 01:21:34 +0500

    debug

--- src/order/urls.py
+++ src/order/urls.py
@@ -1,18 +1,18 @@
-from django.urls import path
-from .views import (
-    order_list,
-    order_detail,
-    order_create,
-    order_update,
-    order_delete,
-    update_order_status
-)
-
-urlpatterns = [
-    path('', order_list, name='order_list'),
-    path('create/', order_create, name='order_create'),
-    path('<int:pk>/', order_detail, name='order_detail'),
-    path('<int:pk>/edit/', order_update, name='order_update'),
-    path('<int:pk>/delete/', order_delete, name='order_delete'),
-    path('<int:pk>/status/<str:status>/', update_order_status, name='update_order_status'),
-]
+# from django.urls import path
+# from .views import (
+#     order_list,
+#     order_detail,
+#     order_create,
+#     order_update,
+#     order_delete,
+#     update_order_status
+# )
+#
+# urlpatterns = [
+#     path('', order_list, name='order_list'),
+#     path('create/', order_create, name='order_create'),
+#     path('<int:pk>/', order_detail, name='order_detail'),
+#     path('<int:pk>/edit/', order_update, name='order_update'),
+#     path('<int:pk>/delete/', order_delete, name='order_delete'),
+#     path('<int:pk>/status/<str:status>/', update_order_status, name='update_order_status'),
+# ]
